c912861d2120ee6a4313bfcde6fe1c32b11d17ac
MarceloSilva0001 <117323231+MarceloSilva0001@users.noreply.github.com>
2025-03-07 20:52:33 -0300

    Fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const path = require('path');
+const bcrypt = require('bcrypt');
 
 const AuthController = {
     register(req, res) {
@@ -10,33 +11,33 @@ const AuthController = {
         res.sendFile(path.join(__dirname, '../views/auth/login.html'));
     },
 
-    authLogin(req, res) {
-        const { email, password } = req.body;  // Pegando os dados do corpo da requisição
+    async authLogin(req, res) {
+        const { email, password } = req.body; // Pegando os dados do corpo da requisição
     
-        // Verificar se o usuário existe
-        User.findOne({ where: { email: email } })  // Agora, o where está corretamente configurado
-            .then(user => {
-                let errorMessage = 'Erro ao fazer Login';
+        try {
+            // Verificar se o usuário existe
+            const user = await User.findOne({ where: { email: email } });
+            let errorMessage = 'Erro ao fazer Login';
     
-                // Verificar se o usuário existe
-                if (!user) {
-                    errorMessage = "E-mail não cadastrado";
-                    return res.redirect(`/login?error=${encodeURIComponent(errorMessage)}`);  // Usando return aqui para interromper a execução
-                }
+            // Verificar se o usuário existe
+            if (!user) {
+                errorMessage = "E-mail não cadastrado";
+                return res.redirect(`/login?error=${encodeURIComponent(errorMessage)}`);
+            }
     
-                // Verificar se a senha corresponde
-                if (user.password !== password) {
-                    errorMessage = "Senha incorreta";
-                    return res.redirect(`/login?error=${encodeURIComponent(errorMessage)}`);  // Usando return aqui também
-                }
+            // Verificar se a senha corresponde usando bcrypt
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) {
+                errorMessage = "Senha incorreta";
+                return res.redirect(`/login?error=${encodeURIComponent(errorMessage)}`);
+            }
     
-                // Senha correta, redireciona para a área logada
-                return res.redirect('/inicio');
-            })
-            .catch(err => {
-                console.error(err);
-                return res.status(500).send('Erro no servidor.');
-            });
+            // Senha correta, redireciona para a área logada
+            return res.redirect('/inicio');
+        } catch (err) {
+            console.error(err);
+            return res.status(500).send('Erro no servidor.');
+        }
     }
     
 };
